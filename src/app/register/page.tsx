296683efe6cef0b25e684d8ee55fe5a4173c3b9d
adrianmcenturion <adrianmcenturion@gmail.com>
2024-08-01 17:08:33 -0300

    fix register page navigation when logged in

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import {redirect} from "next/navigation";
+
+import readUserSession from "@/lib/actions";
+import {privatePaths} from "@/lib/utils";
 
 import Register from "./components/Register";
 
-export default function page() {
+export default async function page() {
+  const {data} = await readUserSession();
+
+  data.session ? redirect(privatePaths.dashboard) : null;
+
   return (
     <div className="mx-auto mt-16 h-full max-w-md">
       <Register />
